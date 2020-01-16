140216bd96fd19ee837b34f9aa25e3d3238efc4f
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-01-16 12:53:49 +0200

    PLaying with async

--- Program.fs
+++ Program.fs
@@ -8,12 +8,26 @@ open Ganss.IO
 let enumerateFiles path =
     Glob.Expand path
 
+let writeFileName (fInfo : System.IO.Abstractions.IFileSystemInfo) =
+    printfn "%s" fInfo.FullName
+
+let reportErrors (fileInfos : Async<seq<System.IO.Abstractions.IFileSystemInfo>>) =
+    printfn "The following files failed:" 
+    async{
+        let! awaitedFinfos = fileInfos
+        return 
+            match awaitedFinfos with
+            | sequence when Seq.isEmpty sequence |> not -> sequence |> Seq.map writeFileName |> (fun _ -> -2)
+            | _ -> 0
+    }
+
 let procssPaths (opts : ParserResult<CmdOptions>) =
     match opts with
     | :? Parsed<CmdOptions> as parsedOpts -> 
         seq {for path in parsedOpts.Value.Files do yield! enumerateFiles path} |>
-            readFiles
-    | _ -> -1
+            readFiles |>
+            reportErrors
+    | _ -> async {return -1}
 
 
 
@@ -22,5 +36,4 @@ let main argv =
     argv 
         |> Parser.Default.ParseArguments<CmdOptions>
         |> procssPaths
-        |> ignore
-    0
+        |> Async.RunSynchronously
--- Reader.fs
+++ Reader.fs
@@ -1,10 +1,29 @@
 ﻿module Reader
 
-let parseFile file =
-    ()
+let parseFile (fInfo : System.IO.Abstractions.IFileSystemInfo) =
+    printfn "%s" fInfo.FullName
+    async {
+        return
+            match fInfo.FullName with
+            | s when s.Length > 10 -> Ok ()
+            | _ -> Error fInfo
+        
+    }
 
-let readFiles files =
-    files 
-        |> Seq.map parseFile
-        |> ignore
-    0
+let readFiles fileInfos =
+    async {
+        let! results = 
+            fileInfos 
+            |> Seq.map parseFile 
+            |> Async.Sequential 
+        
+        
+        return seq {
+            for result in results do
+                match result with
+                | Error e -> yield e
+                | _ -> ()
+        }
+    }
+        
+    
