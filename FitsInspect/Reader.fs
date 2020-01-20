95c1e13f737b2d4e4c8219732c2dd541af866be4
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-01-20 16:25:59 +0200

    Better async support through System.Linq.Async

--- FitsInspect/Reader.fs
+++ FitsInspect/Reader.fs
@@ -1,6 +1,7 @@
 ﻿module Reader
 open System.IO
 open FSharp.Control
+open System.Linq
 
 let parseFile (fInfo : System.IO.Abstractions.IFileSystemInfo) =
     async {
@@ -24,13 +25,39 @@ let parseFile (fInfo : System.IO.Abstractions.IFileSystemInfo) =
             | _ -> return Error fInfo.FullName
     }
 
+let buildKeySequence (i : int) (keys : System.Collections.Immutable.ImmutableList<FitsCs.IFitsValue>) =
+    let init = [sprintf "---------%sBlock #%i:%s---------"  System.Environment.NewLine (i + 1) System.Environment.NewLine]
+    init @ 
+        (keys 
+            |> Seq.mapi (fun ind k -> sprintf "%04i > %s" (ind + 1) (k.ToString(true))) 
+            |> Seq.toList)
+
+let printOutKeys (fInfo : System.IO.Abstractions.IFileSystemInfo) =
+    async {
+        try
+            use fileStream = new FileStream(fInfo.FullName, FileMode.Open, FileAccess.Read)
+            use fitsReader = new FitsCs.FitsReader(fileStream)
+            printfn "%sInspecting file %s%s" System.Environment.NewLine fInfo.Name System.Environment.NewLine
+            let! _ = 
+                fitsReader
+                    .EnumerateBlocksAsync()
+                    .SelectMany((fun block i -> buildKeySequence i block.Keys |> System.Linq.AsyncEnumerable.ToAsyncEnumerable))
+                    .ForEachAsync(printfn "%s")
+                    |> Async.AwaitTask
+
+            return Ok ()
+        with
+            | _ -> return Error fInfo.FullName
+
+    }
+
 let readFiles fileInfos =
     async {
         let! results = 
             fileInfos 
             |> Seq.distinctBy (fun (y : System.IO.Abstractions.IFileSystemInfo) -> y.FullName)
             |> AsyncSeq.ofSeq
-            |> AsyncSeq.mapAsync parseFile
+            |> AsyncSeq.mapAsync printOutKeys
             |> AsyncSeq.toListAsync
         
         
